62dfa61cbfc14d3f9621de3baecf891e954cb043
lofti198 <isolar2005@gmail.com>
2025-10-02 13:15:10 +0300

    redirect homepage to main locale

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,16 @@ export default createMiddleware(routing);
 
 export const config = {
   matcher: [
+    // Enable a redirect to a matching locale at the root
     '/',
-    '/(en|de)/:path*',
-    '/((?!_next|_vercel|api|.*\\..*).*)'
+    
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    '/(de|en)/:path*',
+    
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    '/((?!_next|_vercel|.*\\..*).*)'
   ]
 };
 
